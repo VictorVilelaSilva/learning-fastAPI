5b466e863f9409b5ff499cb2bbfb93c0017136ce
VictorVilelaSilva <victorsouzavilelasilva@gmail.com>
2025-07-31 22:48:45 -0300

    exercicios aula 5

--- tests/test_app.py
+++ tests/test_app.py
@@ -20,17 +20,47 @@ def test_create_user(client):
     }
 
 
+def test_create_user_with_existing_username(client, user):
+    response = client.post(
+        '/users',
+        json={
+            'username': user.username,
+            'email': 'random@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {'detail': 'Username already exists'}
+
+
+def test_create_user_with_existing_email(client, user):
+    response = client.post(
+        '/users',
+        json={
+            'username': 'random',
+            'email': user.email,
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {'detail': 'Email already exists'}
+
+
+def test_get_user(client, user):
+    response = client.get(f'/users/{user.id}')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': user.username,
+        'email': user.email,
+        'id': user.id,
+    }
+
+
 def test_read_users(client):
     response = client.get('/users/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'users': []}
-
-
-def test_read_user(client, user):
-    user_schema = UserPublic.model_validate(user).model_dump()
-    response = client.get('/users/1')
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == user_schema
 
 
 def test_read_user_with_user(client, user):
@@ -105,5 +135,18 @@ def test_delete_user(client, user):
 
 def test_delete_user_not_found(client):
     response = client.delete('/users/999')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'Deu ruim!'}
+
+
+def test_update_user_should_return_not_found__exercicio(client):
+    response = client.put(
+        '/users/666',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'Deu ruim!'}
